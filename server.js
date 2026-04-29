f69b01d24d2d32f6c38e7dbf721b69a63a0d122a
palendra4u-create <palendra4u@gmail.com>
2026-04-29 16:01:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,39 @@
 const express = require("express");
-const app = express();
+const { spawn } = require("child_process");
 
+const app = express();
 const PORT = process.env.PORT || 8080;
 
+const M3U8_URL = "https://air.pc.cdn.bitgravity.com/air/live/pbaudio001/playlist.m3u8";
+
 app.get("/", (req, res) => {
-  res.send("OK");
+  res.send("Relay Running");
+});
+
+app.get("/radio.mp3", (req, res) => {
+  res.setHeader("Content-Type", "audio/mpeg");
+
+  const ffmpeg = spawn("ffmpeg", [
+    "-reconnect", "1",
+    "-reconnect_streamed", "1",
+    "-reconnect_delay_max", "5",
+    "-i", M3U8_URL,
+    "-vn",
+    "-acodec", "libmp3lame",
+    "-b:a", "128k",
+    "-f", "mp3",
+    "pipe:1"
+  ]);
+
+  ffmpeg.stdout.pipe(res);
+
+  ffmpeg.stderr.on("data", data => {
+    console.log(data.toString());
+  });
+
+  req.on("close", () => {
+    ffmpeg.kill("SIGINT");
+  });
 });
 
 app.listen(PORT, "0.0.0.0", () => {
