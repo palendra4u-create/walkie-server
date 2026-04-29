d05c7f0baffc4bca2df83be00abb9e6e10f46052
palendra4u-create <palendra4u@gmail.com>
2026-04-29 14:43:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,31 +4,26 @@ const { spawn } = require("child_process");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Apna M3U8 link yahan daalo
-const M3U8_URL = "https://example.com/live/stream.m3u8";
+const M3U8_URL = "https://air.pc.cdn.bitgravity.com/air/live/pbaudio001/playlist.m3u8";
 
 app.get("/radio.mp3", (req, res) => {
-    res.setHeader("Content-Type", "audio/mpeg");
+  res.setHeader("Content-Type", "audio/mpeg");
 
-    const ffmpeg = spawn("ffmpeg", [
-        "-reconnect", "1",
-        "-reconnect_streamed", "1",
-        "-reconnect_delay_max", "5",
-        "-i", M3U8_URL,
-        "-vn",
-        "-c:a", "libmp3lame",
-        "-b:a", "128k",
-        "-f", "mp3",
-        "pipe:1"
-    ]);
+  const ffmpeg = spawn("ffmpeg", [
+    "-reconnect","1",
+    "-reconnect_streamed","1",
+    "-reconnect_delay_max","5",
+    "-i", M3U8_URL,
+    "-vn",
+    "-acodec","libmp3lame",
+    "-b:a","128k",
+    "-f","mp3",
+    "pipe:1"
+  ]);
 
-    ffmpeg.stdout.pipe(res);
+  ffmpeg.stdout.pipe(res);
 
-    ffmpeg.stderr.on("data", d => console.log(d.toString()));
-
-    req.on("close", () => ffmpeg.kill("SIGINT"));
+  req.on("close", () => ffmpeg.kill("SIGINT"));
 });
 
-app.listen(PORT, () => {
-    console.log(`Running on ${PORT}`);
-});
+app.listen(PORT);
